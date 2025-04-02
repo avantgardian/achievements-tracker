3ae4353440d0788a7fd4821f7846635c5fb0b67e
Andrei Stratu <andrew.avantgardian@gmail.com>
2025-04-02 08:01:43 +0100

    feat: Add Far Cry 3 achievements

---
+++ games/far_cry_3/js/achievements.js
@@ -0,0 +1,310 @@
+window.gameAchievements = [
+    {
+        "id": "FarCry3_Ach_1",
+        "name": "First Blood",
+        "description": "Escape the pirates and survive in the wilderness.",
+        "icon": "images/FarCry3_Ach_1.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_2",
+        "name": "Magic Mushroom",
+        "description": "Return to the doctor with the cave mushrooms.",
+        "icon": "images/FarCry3_Ach_2.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_3",
+        "name": "Worst Date Ever",
+        "description": "Escape the burning hotel.",
+        "icon": "images/FarCry3_Ach_3.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_4",
+        "name": "One of Us",
+        "description": "Complete the Rakyat initiation.",
+        "icon": "images/FarCry3_Ach_4.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_5",
+        "name": "Hands Off My Stoner",
+        "description": "Rescue your friend before he is sold as a slave.",
+        "icon": "images/FarCry3_Ach_5.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_6",
+        "name": "Retake Wall Street",
+        "description": "Rescue your friend from torture.",
+        "icon": "images/FarCry3_Ach_6.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_7",
+        "name": "Have I Told You?",
+        "description": "Discover the definition of insanity.",
+        "icon": "images/FarCry3_Ach_7.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_8",
+        "name": "Taken for Granted",
+        "description": "Survive Vaas’s attack in the compound.",
+        "icon": "images/FarCry3_Ach_8.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_9",
+        "name": "Higher Than a Kite",
+        "description": "Reach the South Island alive. ",
+        "icon": "images/FarCry3_Ach_9.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_10",
+        "name": "Deep Cover",
+        "description": "Don’t blow cover during the interrogation.",
+        "icon": "images/FarCry3_Ach_10.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_11",
+        "name": "Poker Night",
+        "description": "Win the poker game.",
+        "icon": "images/FarCry3_Ach_11.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_12",
+        "name": "What a Trip",
+        "description": "Attend the final ceremony.",
+        "icon": "images/FarCry3_Ach_12.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_13",
+        "name": "Free Fall",
+        "description": "Freefall more than 100m and live (Single Player only).",
+        "icon": "images/FarCry3_Ach_13.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_14",
+        "name": "Inked Up",
+        "description": "Earn 5 skill tattoos.",
+        "icon": "images/FarCry3_Ach_14.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_15",
+        "name": "Fully Inked",
+        "description": "Earn every tattoo by learning all the skills.",
+        "icon": "images/FarCry3_Ach_15.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_16",
+        "name": "Money to Burn",
+        "description": "Spend $5000 at the shop.",
+        "icon": "images/FarCry3_Ach_16.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_17",
+        "name": "Aftermarket Junkie",
+        "description": "Buy all attachments and paint jobs for one weapon.",
+        "icon": "images/FarCry3_Ach_17.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_18",
+        "name": "Rebel With a Cause",
+        "description": "Liberate 3 outposts.",
+        "icon": "images/FarCry3_Ach_18.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_19",
+        "name": "Island Liberator",
+        "description": "Liberate all outposts.",
+        "icon": "images/FarCry3_Ach_19.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_20",
+        "name": "Unheard",
+        "description": "Liberate an outpost without triggering an alarm.",
+        "icon": "images/FarCry3_Ach_20.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_21",
+        "name": "Full Bars",
+        "description": "Activate 9 radio towers.",
+        "icon": "images/FarCry3_Ach_21.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_22",
+        "name": "Archeology 101",
+        "description": "Gather a total of 60 relics.",
+        "icon": "images/FarCry3_Ach_22.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_23",
+        "name": "Dead Letters",
+        "description": "Gather all \"Letters of the Lost\".",
+        "icon": "images/FarCry3_Ach_23.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_24",
+        "name": "Memory to Spare",
+        "description": "Gather all the memory cards.",
+        "icon": "images/FarCry3_Ach_24.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_25",
+        "name": "Jungle Journal",
+        "description": "Unlock 50 entries in the Survival Guide.",
+        "icon": "images/FarCry3_Ach_25.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_26",
+        "name": "Bagged and Tagged",
+        "description": "Complete a Path of the Hunter quest.",
+        "icon": "images/FarCry3_Ach_26.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_27",
+        "name": "Road Trip",
+        "description": "Complete a Supply Drop quest.",
+        "icon": "images/FarCry3_Ach_27.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_28",
+        "name": "In Cold Blood",
+        "description": "Complete a WANTED Dead quest.",
+        "icon": "images/FarCry3_Ach_28.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_29",
+        "name": "Let the Trials Begin",
+        "description": "Beat any Trials of the Rakyat score.",
+        "icon": "images/FarCry3_Ach_29.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_30",
+        "name": "Poker Bully",
+        "description": "Win $1500 playing poker.",
+        "icon": "images/FarCry3_Ach_30.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_31",
+        "name": "Hunter Hunted",
+        "description": "Lure and kill a predator.",
+        "icon": "images/FarCry3_Ach_31.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_32",
+        "name": "Poacher",
+        "description": "Hunt and skin a rare animal.",
+        "icon": "images/FarCry3_Ach_32.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_33",
+        "name": "Artsy Craftsy",
+        "description": "Craft 5 upgrades for your equipment.",
+        "icon": "images/FarCry3_Ach_33.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_34",
+        "name": "Needle Exchange",
+        "description": "Craft 25 syringes.",
+        "icon": "images/FarCry3_Ach_34.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_35",
+        "name": "The Good Stuff",
+        "description": "Craft a Special syringe.",
+        "icon": "images/FarCry3_Ach_35.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_36",
+        "name": "Say Hi to the Internet",
+        "description": "Find the lost Hollywood star.",
+        "icon": "images/FarCry3_Ach_36.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_37",
+        "name": "Heartless Pyro",
+        "description": "Kill 50 enemies with the flamethrower (Single Player only).",
+        "icon": "images/FarCry3_Ach_37.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_38",
+        "name": "Love the Boom",
+        "description": "Kill 4 enemies simultaneously with one explosion (Single Player only).",
+        "icon": "images/FarCry3_Ach_38.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_39",
+        "name": "Rock Always Wins",
+        "description": "Fully distract 25 enemies with rocks (Single Player only).",
+        "icon": "images/FarCry3_Ach_39.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_40",
+        "name": "Never Saw it Coming",
+        "description": "Kill an enemy with a takedown from above from a glider, zipline or parachute (Single Player only).",
+        "icon": "images/FarCry3_Ach_40.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_41",
+        "name": "Improper Use",
+        "description": "Kill an enemy with the Repair Tool (Single Player only).",
+        "icon": "images/FarCry3_Ach_41.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_42",
+        "name": "Toxophilite",
+        "description": "Kill a target from 70m or more with the bow (Single Player only).",
+        "icon": "images/FarCry3_Ach_42.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_43",
+        "name": "Island Paparazzi",
+        "description": "Tag 25 enemies using the camera (Single Player only).",
+        "icon": "images/FarCry3_Ach_43.jpg",
+        "tags": []
+    },
+    {
+        "id": "FarCry3_Ach_44",
+        "name": "Fearless or Stupid",
+        "description": "Dive more than 60m (Single Player only).",
+        "icon": "images/FarCry3_Ach_44.jpg",
+        "tags": []
+    }
+];
