de28bfcba4c62aed897d1666519d4ed5db89c3c7
Andrei Stratu <andrew.avantgardian@gmail.com>
2025-04-03 04:46:45 +0100

    feat: Add SnowRunner achievements

---
+++ games/snowrunner/js/achievements.js
@@ -0,0 +1,261 @@
+window.gameAchievements = [
+    {
+        "id": "1",
+        "name": "Yeah, you can drive!",
+        "description": "All main tutorial hints have been activated at least once",
+        "icon": "images/1.jpg",
+        "tags": []
+    },
+    {
+        "id": "2",
+        "name": "Once a Farmer always a Farmer",
+        "description": "Smash 500 pumpkins",
+        "icon": "images/2.jpg",
+        "tags": []
+    },
+    {
+        "id": "3",
+        "name": "Goliath",
+        "description": "Use a telescopic crane to raise a special objective semi-trailer at least 5 meters above the ground",
+        "icon": "images/3.jpg",
+        "tags": []
+    },
+    {
+        "id": "4",
+        "name": "The Blue Hall",
+        "description": "Drive 1km in the water",
+        "icon": "images/4.jpg",
+        "tags": []
+    },
+    {
+        "id": "5",
+        "name": "Where are the logs?",
+        "description": "Visit every logging area in the game at least once",
+        "icon": "images/5.jpg",
+        "tags": []
+    },
+    {
+        "id": "6",
+        "name": "Play Your Way",
+        "description": "Fix 2000 damage points",
+        "icon": "images/6.jpg",
+        "tags": []
+    },
+    {
+        "id": "7",
+        "name": "Deer Hunt",
+        "description": "Find all upgrades in Michigan",
+        "icon": "images/7.jpg",
+        "tags": []
+    },
+    {
+        "id": "8",
+        "name": "Moose Hunt",
+        "description": "Find all upgrades in Alaska",
+        "icon": "images/8.jpg",
+        "tags": []
+    },
+    {
+        "id": "9",
+        "name": "Bear Hunt",
+        "description": "Find all upgrades in Taymyr",
+        "icon": "images/9.jpg",
+        "tags": []
+    },
+    {
+        "id": "10",
+        "name": "Eat, Sleep, Drill, Repeat",
+        "description": "Deliver all 3 Oil Rigs to their destination points in Alaska",
+        "icon": "images/10.jpg",
+        "tags": []
+    },
+    {
+        "id": "11",
+        "name": "Get over here",
+        "description": "Pull yourself with a winch for at least 6 meters",
+        "icon": "images/11.jpg",
+        "tags": []
+    },
+    {
+        "id": "12",
+        "name": "Through blood &amp; sweat",
+        "description": "Manually load at least 4 standard cargo units in your truck one after another and pack them ",
+        "icon": "images/12.jpg",
+        "tags": []
+    },
+    {
+        "id": "13",
+        "name": "Broken Horse",
+        "description": "Drive 1 km with all wheels broken",
+        "icon": "images/13.jpg",
+        "tags": []
+    },
+    {
+        "id": "14",
+        "name": "Simply Delivered",
+        "description": "Deliver every type of cargo in the game at least once",
+        "icon": "images/14.jpg",
+        "tags": []
+    },
+    {
+        "id": "15",
+        "name": "Workers Unite",
+        "description": "Find both Lenin statues in Taymyr",
+        "icon": "images/15.jpg",
+        "tags": []
+    },
+    {
+        "id": "16",
+        "name": "Tread Softly",
+        "description": "Recover your vehicle 10 times or more",
+        "icon": "images/16.jpg",
+        "tags": []
+    },
+    {
+        "id": "17",
+        "name": "Problem Solved",
+        "description": "Pull a vehicle with a broken engine out of the water with a crane",
+        "icon": "images/17.jpg",
+        "tags": []
+    },
+    {
+        "id": "18",
+        "name": "Untouchable",
+        "description": "Complete any 10 tasks or contests without taking any damage",
+        "icon": "images/18.jpg",
+        "tags": []
+    },
+    {
+        "id": "19",
+        "name": "Gallo-24",
+        "description": "Buy enough upgrades to hit twice the price of the base vehicle you bought them for",
+        "icon": "images/19.jpg",
+        "tags": []
+    },
+    {
+        "id": "20",
+        "name": "18 Wheels is Not Enough",
+        "description": "Own a Azov 42-20 Antarctic",
+        "icon": "images/20.jpg",
+        "tags": []
+    },
+    {
+        "id": "21",
+        "name": "What's a mile?",
+        "description": "Use ZiKZ 5368 to deliver at least 10 cargoes in Michigan or Alaska",
+        "icon": "images/21.jpg",
+        "tags": []
+    },
+    {
+        "id": "22",
+        "name": "Western Wind",
+        "description": "Use Pacific P12 to deliver at least 10 cargoes in Taymyr",
+        "icon": "images/22.jpg",
+        "tags": []
+    },
+    {
+        "id": "23",
+        "name": "Uncle Scrooge",
+        "description": "Earn 100000 currency",
+        "icon": "images/23.jpg",
+        "tags": []
+    },
+    {
+        "id": "24",
+        "name": "\"Stars and Stripes\"",
+        "description": "Own every american vehicle in the game",
+        "icon": "images/24.jpg",
+        "tags": []
+    },
+    {
+        "id": "25",
+        "name": "The Duel",
+        "description": "Driving a red-coloured scout vehicle, collide with any truck and take less damage than that truck",
+        "icon": "images/25.jpg",
+        "tags": []
+    },
+    {
+        "id": "26",
+        "name": "Fuel Economy",
+        "description": "Travel through every region on a single tank of fuel",
+        "icon": "images/26.jpg",
+        "tags": []
+    },
+    {
+        "id": "27",
+        "name": "Victory Parade",
+        "description": "Own every russian vehicle in the game",
+        "icon": "images/27.jpg",
+        "tags": []
+    },
+    {
+        "id": "28",
+        "name": "Dreams Come True",
+        "description": "Repair all the pipes in Alaska",
+        "icon": "images/28.jpg",
+        "tags": []
+    },
+    {
+        "id": "29",
+        "name": "\"All Along the Watchtower\"",
+        "description": "Explore all watchtowers in the game",
+        "icon": "images/29.jpg",
+        "tags": []
+    },
+    {
+        "id": "30",
+        "name": "All Starts From a Garage",
+        "description": "Explore all garages in the game",
+        "icon": "images/30.jpg",
+        "tags": []
+    },
+    {
+        "id": "31",
+        "name": "Pedal to the Metal",
+        "description": "Travel from one gateway to another on one map without releasing the accelerator",
+        "icon": "images/31.jpg",
+        "tags": []
+    },
+    {
+        "id": "32",
+        "name": "Bering Strait",
+        "description": "Own a TUZ 420 \"Tatarin\" and have it stationed in any garage in Alaska",
+        "icon": "images/32.jpg",
+        "tags": []
+    },
+    {
+        "id": "33",
+        "name": "Convoy",
+        "description": "Transport a winched vehicle with a broken engine from one map to another and put it into a garage",
+        "icon": "images/33.jpg",
+        "tags": []
+    },
+    {
+        "id": "34",
+        "name": "Workaholic",
+        "description": "Complete every contract in the game",
+        "icon": "images/34.jpg",
+        "tags": []
+    },
+    {
+        "id": "35",
+        "name": "Model Collector",
+        "description": "Own every vehicle in the game",
+        "icon": "images/35.jpg",
+        "tags": []
+    },
+    {
+        "id": "36",
+        "name": "Ain't no rest for the...trucker?",
+        "description": "Complete every task and contest in the game",
+        "icon": "images/36.jpg",
+        "tags": []
+    },
+    {
+        "id": "37",
+        "name": "The Black Shuck",
+        "description": "Drive through 1000 km",
+        "icon": "images/37.jpg",
+        "tags": []
+    }
+];
