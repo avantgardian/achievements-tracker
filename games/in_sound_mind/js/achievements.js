38c7369f397bd6f42b9794b670abf1e18da862e1
Andrei Stratu <andrew.avantgardian@gmail.com>
2025-04-10 01:31:35 +0100

    Added In sound mind

---
+++ games/in_sound_mind/js/achievements.js
@@ -0,0 +1,226 @@
+window.gameAchievements = [
+    {
+        "id": "IsmCompleteC1",
+        "name": "Mirror, Mirror, on the Wall...",
+        "description": "Finish Tape 1 ",
+        "icon": "images/IsmCompleteC1.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmCompleteC2",
+        "name": "Lights Out!",
+        "description": "Finish Tape 2",
+        "icon": "images/IsmCompleteC2.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmCompleteC3",
+        "name": "Take a Chill Pill",
+        "description": "Finish Tape 3",
+        "icon": "images/IsmCompleteC3.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmCompleteC4",
+        "name": "Flashback",
+        "description": "Finish Tape 4 ",
+        "icon": "images/IsmCompleteC4.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmCompleteC5",
+        "name": "Improvise. Adapt. Overcome",
+        "description": "",
+        "icon": "images/IsmCompleteC5.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmCompleteGame",
+        "name": "Patients and Grace",
+        "description": "",
+        "icon": "images/IsmCompleteGame.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmAllPillsC1",
+        "name": "Pill Prescriber",
+        "description": "Collect all pills in the supermarket level",
+        "icon": "images/IsmAllPillsC1.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmAllPillsC2",
+        "name": "Pill Pusher",
+        "description": "Collect all pills in the lighthouse level",
+        "icon": "images/IsmAllPillsC2.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmAllPillsC3",
+        "name": "Pill Prospector ",
+        "description": "Collect all pills in the factory level",
+        "icon": "images/IsmAllPillsC3.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmAllPillsC4",
+        "name": "Pill Popper",
+        "description": "Collect all pills in the forest level",
+        "icon": "images/IsmAllPillsC4.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmAllPillsHub",
+        "name": "Pharma Bro",
+        "description": "Collect all pills in your building",
+        "icon": "images/IsmAllPillsHub.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmAllRecords",
+        "name": "Audiophile",
+        "description": "Collect all vinyl records",
+        "icon": "images/IsmAllRecords.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmPetCat",
+        "name": "GOTY 10/10",
+        "description": "Pet Tonia",
+        "icon": "images/IsmPetCat.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmHangUp",
+        "name": "Nope.",
+        "description": "Hang up on your sneaky stalker",
+        "icon": "images/IsmHangUp.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmGetPistol",
+        "name": "Point and Shoot",
+        "description": "Acquire the Pistol in your building",
+        "icon": "images/IsmGetPistol.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmGetGasMask",
+        "name": "Breathing Exercises",
+        "description": "Acquire the Gas Mask in your building",
+        "icon": "images/IsmGetGasMask.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmGetShotgun",
+        "name": "Scatterbrain",
+        "description": "Acquire the Shotgun in your building",
+        "icon": "images/IsmGetShotgun.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmC1Cart",
+        "name": "Shopping for Achievements",
+        "description": "Push a cart into the water on the way to Homa Mart",
+        "icon": "images/IsmC1Cart.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmC1Stickynote",
+        "name": "Turn that Frown Upside Down",
+        "description": "Give a mannequin a face",
+        "icon": "images/IsmC1Stickynote.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmC1WatcherMirror",
+        "name": "You're a Mean One...",
+        "description": "Scare The Watcher away using the Mirror Shard",
+        "icon": "images/IsmC1WatcherMirror.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmC2Crow",
+        "name": "A Murder of Crows",
+        "description": "Shoot a crow",
+        "icon": "images/IsmC2Crow.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmC2ShadeHeadshot",
+        "name": "Take That!",
+        "description": "Damage The Shade using a weapon",
+        "icon": "images/IsmC2ShadeHeadshot.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmC2ShadeProjectile",
+        "name": "Give Me All You Got!",
+        "description": "Shoot down a Shade projectile",
+        "icon": "images/IsmC2ShadeProjectile.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmC3BullRide",
+        "name": "Rodeo Clown",
+        "description": "Ride The Bull for 5 seconds",
+        "icon": "images/IsmC3BullRide.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmC3Lab",
+        "name": "Access Granted",
+        "description": "",
+        "icon": "images/IsmC3Lab.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmC3BullRunOver",
+        "name": "Ole!",
+        "description": "Trick The Bull into attacking an inkblot",
+        "icon": "images/IsmC3BullRunOver.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmC4Beacons",
+        "name": "Good Listener",
+        "description": "Find and listen to all of Lucas' session fragments",
+        "icon": "images/IsmC4Beacons.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmC4EmptySong",
+        "name": "Unsung Hero",
+        "description": "",
+        "icon": "images/IsmC4EmptySong.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmC4FlashStun",
+        "name": "Feedback Loop\t",
+        "description": "Stun The Flash using your Radio Device",
+        "icon": "images/IsmC4FlashStun.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmC5HighFive",
+        "name": "High Five for Dummies",
+        "description": "Give Dave a high five while fighting for your mind",
+        "icon": "images/IsmC5HighFive.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmAllWeapons",
+        "name": "Fully Loaded",
+        "description": "Find all weapons in the game",
+        "icon": "images/IsmAllWeapons.jpg",
+        "tags": []
+    },
+    {
+        "id": "IsmAllPills",
+        "name": "Well Medicated",
+        "description": "Find all pills in the game",
+        "icon": "images/IsmAllPills.jpg",
+        "tags": []
+    }
+];
