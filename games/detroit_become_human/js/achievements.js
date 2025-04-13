d29053c6f8a949b911cb5d2100c034e55b82de02
Andrei Stratu <andrew.avantgardian@gmail.com>
2025-04-13 04:51:32 +0100

    Add Detroit: Become Human with complete achievement descriptions

---
+++ games/detroit_become_human/js/achievements.js
@@ -0,0 +1,338 @@
+window.gameAchievements = [
+    {
+        "id": "48",
+        "name": "SURVIVORS",
+        "description": "Complete the game with all three main characters alive",
+        "icon": "images/48.jpg",
+        "tags": []
+    },
+    {
+        "id": "1",
+        "name": "THANK YOU",
+        "description": "Play the first chapter",
+        "icon": "images/1.jpg",
+        "tags": []
+    },
+    {
+        "id": "47",
+        "name": "THESE ARE OUR STORIES",
+        "description": "Complete all magazine collections",
+        "icon": "images/47.jpg",
+        "tags": []
+    },
+    {
+        "id": "2",
+        "name": "MISSION ACCOMPLISHED",
+        "description": "Complete the game",
+        "icon": "images/2.jpg",
+        "tags": []
+    },
+    {
+        "id": "3",
+        "name": "SECRETS",
+        "description": "Find all magazines",
+        "icon": "images/3.jpg",
+        "tags": []
+    },
+    {
+        "id": "4",
+        "name": "DEVIANT LOCATED",
+        "description": "Find the deviant in the 'Partners' chapter",
+        "icon": "images/4.jpg",
+        "tags": []
+    },
+    {
+        "id": "5",
+        "name": "WE ARE FREE",
+        "description": "Successfully escape the recycling center",
+        "icon": "images/5.jpg",
+        "tags": []
+    },
+    {
+        "id": "6",
+        "name": "DEFEND YOURSELF",
+        "description": "Successfully defend yourself against the police in 'Fugitives'",
+        "icon": "images/6.jpg",
+        "tags": []
+    },
+    {
+        "id": "7",
+        "name": "SELF-CONTROL",
+        "description": "Complete the game without killing anyone",
+        "icon": "images/7.jpg",
+        "tags": []
+    },
+    {
+        "id": "8",
+        "name": "CONFESSION",
+        "description": "Make the deviant confess in 'Partners'",
+        "icon": "images/8.jpg",
+        "tags": []
+    },
+    {
+        "id": "9",
+        "name": "SHELTER",
+        "description": "Find shelter for Alice and Kara in 'Fugitives'",
+        "icon": "images/9.jpg",
+        "tags": []
+    },
+    {
+        "id": "10",
+        "name": "KNOW YOUR PARTNER",
+        "description": "Complete all dialogues with Hank",
+        "icon": "images/10.jpg",
+        "tags": []
+    },
+    {
+        "id": "11",
+        "name": "RUN KARA RUN",
+        "description": "Successfully escape the police in 'Fugitives'",
+        "icon": "images/11.jpg",
+        "tags": []
+    },
+    {
+        "id": "12",
+        "name": "CATCH IT",
+        "description": "Catch the fish in 'The Nest'",
+        "icon": "images/12.jpg",
+        "tags": []
+    },
+    {
+        "id": "13",
+        "name": "SAVE HANK",
+        "description": "Save Hank from falling off the roof",
+        "icon": "images/13.jpg",
+        "tags": []
+    },
+    {
+        "id": "14",
+        "name": "ESCAPE THE MANOR",
+        "description": "Successfully escape Zlatko's manor",
+        "icon": "images/14.jpg",
+        "tags": []
+    },
+    {
+        "id": "15",
+        "name": "RUTHLESS",
+        "description": "Complete the game by killing all main characters",
+        "icon": "images/15.jpg",
+        "tags": []
+    },
+    {
+        "id": "16",
+        "name": "DOUBTS",
+        "description": "Make Connor doubt his mission",
+        "icon": "images/16.jpg",
+        "tags": []
+    },
+    {
+        "id": "17",
+        "name": "JERICHO'S HERO",
+        "description": "Save Markus during the raid on Jericho",
+        "icon": "images/17.jpg",
+        "tags": []
+    },
+    {
+        "id": "18",
+        "name": "JUST A MACHINE",
+        "description": "Complete the game without any character becoming deviant",
+        "icon": "images/18.jpg",
+        "tags": []
+    },
+    {
+        "id": "19",
+        "name": "A SMILE ON HER FACE",
+        "description": "Make Alice smile in 'Fugitives'",
+        "icon": "images/19.jpg",
+        "tags": []
+    },
+    {
+        "id": "20",
+        "name": "WHEN A PLAN COMES TOGETHER",
+        "description": "Successfully complete the heist in 'The Stratford Tower'",
+        "icon": "images/20.jpg",
+        "tags": []
+    },
+    {
+        "id": "21",
+        "name": "A GLIMPSE OF JERICHO",
+        "description": "Find Jericho",
+        "icon": "images/21.jpg",
+        "tags": []
+    },
+    {
+        "id": "22",
+        "name": "SEND A MESSAGE",
+        "description": "Successfully broadcast the message in 'The Stratford Tower'",
+        "icon": "images/22.jpg",
+        "tags": []
+    },
+    {
+        "id": "23",
+        "name": "BURN THE PLACE",
+        "description": "Burn down the evidence in 'The Nest'",
+        "icon": "images/23.jpg",
+        "tags": []
+    },
+    {
+        "id": "24",
+        "name": "NOTHING TO SEE HERE",
+        "description": "Successfully hide from the police in 'Fugitives'",
+        "icon": "images/24.jpg",
+        "tags": []
+    },
+    {
+        "id": "25",
+        "name": "CONFRONTATION",
+        "description": "Confront the deviant in 'The Nest'",
+        "icon": "images/25.jpg",
+        "tags": []
+    },
+    {
+        "id": "26",
+        "name": "STAND YOUR GROUND",
+        "description": "Successfully defend Jericho",
+        "icon": "images/26.jpg",
+        "tags": []
+    },
+    {
+        "id": "27",
+        "name": "PRIORITIES",
+        "description": "Save Hank instead of chasing the deviant",
+        "icon": "images/27.jpg",
+        "tags": []
+    },
+    {
+        "id": "28",
+        "name": "KINSHIP",
+        "description": "Develop a relationship with Hank",
+        "icon": "images/28.jpg",
+        "tags": []
+    },
+    {
+        "id": "29",
+        "name": "BLOODHOUND",
+        "description": "Find all the deviants in 'The Nest'",
+        "icon": "images/29.jpg",
+        "tags": []
+    },
+    {
+        "id": "30",
+        "name": "THREE AT JERICHO",
+        "description": "Have all three main characters reach Jericho",
+        "icon": "images/30.jpg",
+        "tags": []
+    },
+    {
+        "id": "31",
+        "name": "SCORCHED EARTH",
+        "description": "Complete the game by destroying all evidence",
+        "icon": "images/31.jpg",
+        "tags": []
+    },
+    {
+        "id": "32",
+        "name": "LIBERATION",
+        "description": "Successfully lead the android revolution",
+        "icon": "images/32.jpg",
+        "tags": []
+    },
+    {
+        "id": "33",
+        "name": "MORAL VICTORY",
+        "description": "Complete the game by winning the moral argument",
+        "icon": "images/33.jpg",
+        "tags": []
+    },
+    {
+        "id": "34",
+        "name": "ESCAPE DEATH",
+        "description": "Survive all the QTE sequences in 'The Hostage'",
+        "icon": "images/34.jpg",
+        "tags": []
+    },
+    {
+        "id": "35",
+        "name": "SAFE HARBOR",
+        "description": "",
+        "icon": "images/35.jpg",
+        "tags": []
+    },
+    {
+        "id": "36",
+        "name": "AN ARMY OF ME",
+        "description": "",
+        "icon": "images/36.jpg",
+        "tags": []
+    },
+    {
+        "id": "37",
+        "name": "MISSION COMPLETE",
+        "description": "",
+        "icon": "images/37.jpg",
+        "tags": []
+    },
+    {
+        "id": "38",
+        "name": "MY TURN TO DECIDE",
+        "description": "",
+        "icon": "images/38.jpg",
+        "tags": []
+    },
+    {
+        "id": "39",
+        "name": "COMPLIANT",
+        "description": "",
+        "icon": "images/39.jpg",
+        "tags": []
+    },
+    {
+        "id": "40",
+        "name": "ONE OF US",
+        "description": "",
+        "icon": "images/40.jpg",
+        "tags": []
+    },
+    {
+        "id": "41",
+        "name": "UNDEFEATED",
+        "description": "",
+        "icon": "images/41.jpg",
+        "tags": []
+    },
+    {
+        "id": "42",
+        "name": "BOOKWORM",
+        "description": "",
+        "icon": "images/42.jpg",
+        "tags": []
+    },
+    {
+        "id": "43",
+        "name": "PARTNERS",
+        "description": "",
+        "icon": "images/43.jpg",
+        "tags": []
+    },
+    {
+        "id": "44",
+        "name": "HAPPY FAMILY",
+        "description": "",
+        "icon": "images/44.jpg",
+        "tags": []
+    },
+    {
+        "id": "45",
+        "name": "I'LL BE BACK",
+        "description": "",
+        "icon": "images/45.jpg",
+        "tags": []
+    },
+    {
+        "id": "46",
+        "name": "THIS IS MY STORY",
+        "description": "",
+        "icon": "images/46.jpg",
+        "tags": []
+    }
+];
