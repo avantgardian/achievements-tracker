d7c55598dfab42ff94271083b62bf7c1ad7998db
Andrei Stratu <andrew.avantgardian@gmail.com>
2025-04-14 23:40:50 +0100

    Add Pathfinder: Kingmaker achievements with complete descriptions

---
+++ games/pathfinder_kingmaker_—_enhanced_plus_edition/js/achievements.js
@@ -0,0 +1,520 @@
+window.gameAchievements = [
+    {
+        "id": "ACH_assassins",
+        "name": "Assassinating the Assassins",
+        "description": "You survived the night attack on Jamandi Aldori's mansion. What a nice way to start your adventure!",
+        "icon": "images/ACH_assassins.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_NoCountryForBandits",
+        "name": "No Country for Bandits",
+        "description": "Clear the Stolen lands from bandit gangs and defeat the self-proclaimed bandit king.",
+        "icon": "images/ACH_NoCountryForBandits.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_Foundation",
+        "name": "Foundation",
+        "description": "You founded a barony in a wild, dangerous land. Good luck with that!",
+        "icon": "images/ACH_Foundation.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_FireOrAcid",
+        "name": "Fire. Or Acid",
+        "description": "Stop the troll invasion.",
+        "icon": "images/ACH_FireOrAcid.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_EnemyWithin",
+        "name": "Enemy Within",
+        "description": "Solve the mystery and save your barony from an otherworldly threat.",
+        "icon": "images/ACH_EnemyWithin.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_VFor",
+        "name": "V for...",
+        "description": "Silence the voice of the lost empire.",
+        "icon": "images/ACH_VFor.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_TwiceBorn",
+        "name": "Twice-born, Twice-beaten",
+        "description": "Deal with the barbarian horde on your borders.",
+        "icon": "images/ACH_TwiceBorn.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_Hail",
+        "name": "Hail to the King!",
+        "description": "You are now the one and only ruler of your very own kingdom!",
+        "icon": "images/ACH_Hail.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_NeighborlyAffairs",
+        "name": "Neighborly Affairs",
+        "description": "Deal with your fellow neighbor king and his appetite for your land.",
+        "icon": "images/ACH_NeighborlyAffairs.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_against",
+        "name": "Against All Odds",
+        "description": "Travel to the mysterious realm of the fey and face your destiny.",
+        "icon": "images/ACH_against.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_Flaming",
+        "name": "Flaming Crown",
+        "description": "Face and defeat the one final enemy.",
+        "icon": "images/ACH_Flaming.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_FirstResults",
+        "name": "First Results",
+        "description": "Increase the rank of at least one kingdom stat.",
+        "icon": "images/ACH_FirstResults.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_Useful",
+        "name": "Useful Instruments",
+        "description": "Unlock all five additional kingdom stats and assign leaders to respective positions.",
+        "icon": "images/ACH_Useful.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_ProsperousKingdom",
+        "name": "Prosperous Kingdom",
+        "description": "Raise all kingdom stats to the 10th rank.",
+        "icon": "images/ACH_ProsperousKingdom.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_FirstInCollection",
+        "name": "First in Collection",
+        "description": "Obtain an outstanding gift from one of your royal artisans.",
+        "icon": "images/ACH_FirstInCollection.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_admirer",
+        "name": "Admirer of Finer Things",
+        "description": "Gather masterpiece works from all ten royal artisans.",
+        "icon": "images/ACH_admirer.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_Friend",
+        "name": "Friend of Many",
+        "description": "Succeed in three 'Kingdom as Companion' quests.",
+        "icon": "images/ACH_Friend.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_conqueror",
+        "name": "Conqueror",
+        "description": "Claim all available regions.",
+        "icon": "images/ACH_conqueror.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_ProsperousConqueror",
+        "name": "Prosperous Conqueror",
+        "description": "Claim all available regions and develop them to their maximum potential.",
+        "icon": "images/ACH_ProsperousConqueror.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_Intensive",
+        "name": "Intensive Development",
+        "description": "Explore the Stolen Lands and find all resource locations.",
+        "icon": "images/ACH_Intensive.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_Opportunist",
+        "name": "Opportunist",
+        "description": "Successfully solve 100 'opportunity' kingdom events.",
+        "icon": "images/ACH_Opportunist.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_Troubleshooter",
+        "name": "Troubleshooter",
+        "description": "Successfully solve 100 'problem' kingdom events.",
+        "icon": "images/ACH_Troubleshooter.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_RoyalResque",
+        "name": "Royal Rescue Service",
+        "description": "Find and save Tig, poor lost child.",
+        "icon": "images/ACH_RoyalResque.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_BirdOfPrey",
+        "name": "Bird of Prey",
+        "description": "Win the main prize of the royal hunt fair and square, killing two magical beasts.",
+        "icon": "images/ACH_BirdOfPrey.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_DealWithDevil",
+        "name": "Deal with the Devil",
+        "description": "Win the favor of the Order of the Rack. Or of their enemy.",
+        "icon": "images/ACH_DealWithDevil.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_bloody",
+        "name": "Bloody Squirrels!",
+        "description": "Get the grimoire from one remarkable hermit mage.",
+        "icon": "images/ACH_bloody.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_TheWiseWay",
+        "name": "The Wise Way",
+        "description": "Win the favor of the most mysterious mentor from the most mysterious monk order.",
+        "icon": "images/ACH_TheWiseWay.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_Followers",
+        "name": "Followers",
+        "description": "Recruit all available companions.",
+        "icon": "images/ACH_Followers.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_camrades",
+        "name": "Comrades-in-Arms",
+        "description": "Succeed in all available companion quests and win the loyalty of your comrades.",
+        "icon": "images/ACH_camrades.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_TaleOf",
+        "name": "Tale Of Days Long Gone",
+        "description": "Collect all pieces of an ancient artifact and let the Storyteller reforge it for you.",
+        "icon": "images/ACH_TaleOf.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_ThosePrecious",
+        "name": "Those Precious Tales",
+        "description": "Gather all Storyteller's artifacts.",
+        "icon": "images/ACH_ThosePrecious.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_InMyHeart",
+        "name": "In My Heart",
+        "description": "Find your love.",
+        "icon": "images/ACH_InMyHeart.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_YetAnother",
+        "name": "Yet Another Ancient Evil",
+        "description": "Find and defeat the secret threat, locked in a magical prison in The Stolen Lands.",
+        "icon": "images/ACH_YetAnother.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_Survivor",
+        "name": "Survivor",
+        "description": "Live through 100 random attacks in the wild.",
+        "icon": "images/ACH_Survivor.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_TheStoryEnd",
+        "name": "The Story's End",
+        "description": "Win the game!",
+        "icon": "images/ACH_TheStoryEnd.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_MoreChallenge",
+        "name": "More Challenge, Please",
+        "description": "Win the game, playing on 'Challenging rules' difficulty or above.",
+        "icon": "images/ACH_MoreChallenge.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_IronKing",
+        "name": "Iron King",
+        "description": "Win the game, playing on Last Azlanti mode.",
+        "icon": "images/ACH_IronKing.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_ForgedInBlood",
+        "name": "Forged in Blood",
+        "description": "Win the game, playing on Unfair difficulty.",
+        "icon": "images/ACH_ForgedInBlood.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_OnePunch",
+        "name": "One Punch",
+        "description": "Deal more than 150 damage with a single unarmed attack.",
+        "icon": "images/ACH_OnePunch.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_NowYouFall",
+        "name": "Now You Fall!",
+        "description": "Knock down any creature that is 3 size categories larger than you.",
+        "icon": "images/ACH_NowYouFall.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_ZenArcher",
+        "name": "Zen Archer",
+        "description": "Inflict critical damage with a bow or crossbow while blinded.",
+        "icon": "images/ACH_ZenArcher.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_Metamagister",
+        "name": "Metamagister",
+        "description": "Use in battle any spell with 5 metamagic modifications applied.",
+        "icon": "images/ACH_Metamagister.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_Slayer",
+        "name": "Slayer of Bears",
+        "description": "In chapter 1, kill the bear in the Temple of the Elk with single level 2 character, playing on Hard difficulty or above.",
+        "icon": "images/ACH_Slayer.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_Sinmarket",
+        "name": "Sinmarket's Best Customer",
+        "description": "Make the fey merchant adore you.",
+        "icon": "images/ACH_Sinmarket.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_Goblin",
+        "name": "Goblin Tears",
+        "description": "Make the goblin merchant cry real tears.",
+        "icon": "images/ACH_Goblin.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_God",
+        "name": "God of Poetry",
+        "description": "Help the goblins compose their warsong!",
+        "icon": "images/ACH_God.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_SeeYou",
+        "name": "See You Yesterday!",
+        "description": "Meet the legendary Eldest of Time three times. Or, rather, in three different times.",
+        "icon": "images/ACH_SeeYou.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_NothingLeft",
+        "name": "Nothing Left Behind",
+        "description": "Leave any map, carrying 5000 lbs. without being encumbered.",
+        "icon": "images/ACH_NothingLeft.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_SoShallYou",
+        "name": "So Shall You Reap",
+        "description": "Complete the quest 'The Price of Curiosity' and deal with the consequences of your actions.",
+        "icon": "images/ACH_SoShallYou.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_TricksOfTime",
+        "name": "Tricks of Time",
+        "description": "Successfully complete all time-related puzzles in the House at the Edge of Time.",
+        "icon": "images/ACH_TricksOfTime.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_WorshipMe",
+        "name": "Worship Me!",
+        "description": "Achieve maximum favor with one of the deities in the game.",
+        "icon": "images/ACH_WorshipMe.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_FoesNoMore",
+        "name": "Foes No More",
+        "description": "Resolve the conflict between the Technic League and the First World in a peaceful manner.",
+        "icon": "images/ACH_FoesNoMore.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_KnowWeakness",
+        "name": "Know Their Weaknesses",
+        "description": "Successfully identify and exploit the weaknesses of 50 different types of enemies.",
+        "icon": "images/ACH_KnowWeakness.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_TheGoodGuy",
+        "name": "The Good Guy",
+        "description": "Complete the game with a Lawful Good alignment.",
+        "icon": "images/ACH_TheGoodGuy.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_Evil",
+        "name": "Evil Is My Name",
+        "description": "Complete the game with a Chaotic Evil alignment.",
+        "icon": "images/ACH_Evil.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_TakingNoSides",
+        "name": "Taking No Sides",
+        "description": "Maintain a True Neutral alignment throughout the entire game.",
+        "icon": "images/ACH_TakingNoSides.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_InTheName",
+        "name": "In The Name Of The Law",
+        "description": "Complete the game with a Lawful Neutral alignment.",
+        "icon": "images/ACH_InTheName.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_TheyCallMe",
+        "name": "They Call Me Unpredictable",
+        "description": "Complete the game with a Chaotic Neutral alignment.",
+        "icon": "images/ACH_TheyCallMe.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_Notwhatitseems",
+        "name": "Not What It Seems",
+        "description": "Help the mysterious tiefling guest in her hunt for the ancient treasure.",
+        "icon": "images/ACH_Notwhatitseems.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_patricide",
+        "name": "Patricide",
+        "description": "Slay the Forefather.",
+        "icon": "images/ACH_patricide.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_Thetieflingtrio",
+        "name": "The Tiefling Trio",
+        "description": "Complete all companion quests for the tiefling characters (Octavia, Regongar, and Nok-Nok).",
+        "icon": "images/ACH_Thetieflingtrio.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_AjobforaGeneral",
+        "name": "A Job For A General",
+        "description": "Resolve the first crisis in Varnhold's history.",
+        "icon": "images/ACH_AjobforaGeneral.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_Thatsyourlot",
+        "name": "That's Your Lot",
+        "description": "Complete the \"Varnhold's Lot\" DLC campaign.",
+        "icon": "images/ACH_Thatsyourlot.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_OnTheTrail",
+        "name": "Lost And Found",
+        "description": "Visit Lostlarn Keep while playing the main story campaign.",
+        "icon": "images/ACH_OnTheTrail.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_AndTheMurdererIs",
+        "name": "And The Murderer Is...",
+        "description": "Uncover the secrets of the Blackstones Ford inn.",
+        "icon": "images/ACH_AndTheMurdererIs.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_Theroadgoeseveron",
+        "name": "The Road Goes Ever On",
+        "description": "Complete all available side quests in the game.",
+        "icon": "images/ACH_Theroadgoeseveron.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_Whosthere",
+        "name": "Who's There?",
+        "description": "Successfully complete all perception checks in the game.",
+        "icon": "images/ACH_Whosthere.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_ApocalypseCanWait",
+        "name": "Apocalypse Can Wait",
+        "description": "Find out the truth and eliminate the source of the threat Beneath the Stolen Lands while playing in rogue-like mode.",
+        "icon": "images/ACH_ApocalypseCanWait.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_ThePriceYouPay",
+        "name": "The Price You Pay",
+        "description": "Find out the truth and eliminate the source of the threat Beneath the Stolen Lands while playing the main campaign.",
+        "icon": "images/ACH_ThePriceYouPay.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_Descent",
+        "name": "Descent",
+        "description": "Reach the 10th dungeon level Beneath the Stolen Lands while playing in rogue-like mode.",
+        "icon": "images/ACH_Descent.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_IntotheDarkness",
+        "name": "Into the Darkness",
+        "description": "Reach the 30th dungeon level Beneath the Stolen Lands while playing in rogue-like mode.",
+        "icon": "images/ACH_IntotheDarkness.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_DepthsOfMadness",
+        "name": "Depths Of Madness",
+        "description": "Reach the 50th dungeon level Beneath the Stolen Lands while playing in rogue-like mode.",
+        "icon": "images/ACH_DepthsOfMadness.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_HonestLooting",
+        "name": "Honest Looting",
+        "description": "Help the Honest Guy to expand his assortment of goods by cleaning the dungeon from the most dangerous monsters.\t",
+        "icon": "images/ACH_HonestLooting.jpg",
+        "tags": []
+    },
+    {
+        "id": "ACH_AMemorableMoment",
+        "name": "A Memorable Moment",
+        "description": "Die for the first time while playing 'Beneath the Stolen Lands' in a rogue-like mode.",
+        "icon": "images/ACH_AMemorableMoment.jpg",
+        "tags": []
+    }
+];
