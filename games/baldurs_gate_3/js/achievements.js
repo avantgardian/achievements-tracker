e8b26a6730a1fc09a8f3131eabae2942a45b69b9
Andrei Stratu <andrew.avantgardian@gmail.com>
2025-04-16 23:47:31 +0100

    Add Baldur's Gate 3 achievements and update main index

---
+++ games/baldurs_gate_3/js/achievements.js
@@ -0,0 +1,380 @@
+window.gameAchievements = [
+    {
+        "id": "BG3_Quest01",
+        "name": "Descent From Avernus",
+        "description": "Take control of the nautiloid and escape the Hells.",
+        "icon": "images/BG3_Quest01.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest02",
+        "name": "The Plot Thickens",
+        "description": "",
+        "icon": "images/BG3_Quest02.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest03",
+        "name": "The City Awaits",
+        "description": "",
+        "icon": "images/BG3_Quest03.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest04",
+        "name": "All's Well That Ends Well",
+        "description": "",
+        "icon": "images/BG3_Quest04.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest05",
+        "name": "Absolute Power Corrupts",
+        "description": "",
+        "icon": "images/BG3_Quest05.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest06",
+        "name": "Hero of the Forgotten Realms",
+        "description": "",
+        "icon": "images/BG3_Quest06.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest07",
+        "name": "Sins of the Father",
+        "description": "",
+        "icon": "images/BG3_Quest07.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest08",
+        "name": "Ceremorphosis",
+        "description": "",
+        "icon": "images/BG3_Quest08.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest09",
+        "name": "Roleplayer",
+        "description": "Complete ten background goals in a single playthrough - you are one with your character.",
+        "icon": "images/BG3_Quest09.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest10",
+        "name": "Bedrolls and Breakfast",
+        "description": "Take four full Long Rests in a single playthrough - adventuring's tiring work.",
+        "icon": "images/BG3_Quest10.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest11",
+        "name": "Expand Your Mind",
+        "description": "",
+        "icon": "images/BG3_Quest11.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest12",
+        "name": "Dig for Victory",
+        "description": "Dig up five buried chests in a single playthrough - treasure! ",
+        "icon": "images/BG3_Quest12.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest13",
+        "name": "No Penny Required",
+        "description": "Successfully use Detect Thoughts to pry into someone's thoughts. ",
+        "icon": "images/BG3_Quest13.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest14",
+        "name": "Escapologist",
+        "description": "Break out of prison after being arrested - aren't you daring?",
+        "icon": "images/BG3_Quest14.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest15",
+        "name": "Outsourcing",
+        "description": "Recruit a hireling. You can befriend them or use them as cannon-fodder - we won't judge.",
+        "icon": "images/BG3_Quest15.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest16",
+        "name": "Jack-of-all-Trades",
+        "description": "Multiclass into every class in one playthrough without asking Withers to change your character.",
+        "icon": "images/BG3_Quest16.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest17",
+        "name": "Homebrewer",
+        "description": "Create three unique alchemical solutions in a single playthrough - bottoms up! ",
+        "icon": "images/BG3_Quest17.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest18",
+        "name": "You Have Two Hands for a Reason",
+        "description": "",
+        "icon": "images/BG3_Quest18.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest19",
+        "name": "Rude, Crude, and Full of Attitude",
+        "description": "",
+        "icon": "images/BG3_Quest19.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest20",
+        "name": "Forged in Blood and Fire",
+        "description": "",
+        "icon": "images/BG3_Quest20.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest21",
+        "name": "Under Lock and Key",
+        "description": "",
+        "icon": "images/BG3_Quest21.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest22",
+        "name": "She Cannot Be Caged!",
+        "description": "",
+        "icon": "images/BG3_Quest22.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest23",
+        "name": "Taking Blood",
+        "description": "",
+        "icon": "images/BG3_Quest23.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest24",
+        "name": "Leave No One Behind",
+        "description": "",
+        "icon": "images/BG3_Quest24.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest25",
+        "name": "Murder in Baldur's Gate",
+        "description": "",
+        "icon": "images/BG3_Quest25.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest26",
+        "name": "Mind Blown",
+        "description": "",
+        "icon": "images/BG3_Quest26.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest27",
+        "name": "Kill Two Birds With One Gnome",
+        "description": "Use one enemy as an improvised weapon against another. ",
+        "icon": "images/BG3_Quest27.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest28",
+        "name": "Busker",
+        "description": "Earn a hundred gold from playing sweet, sweet music in a single playthrough. ",
+        "icon": "images/BG3_Quest28.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest29",
+        "name": "Action Surge",
+        "description": "Perform five attacks in one turn. Your enemies won't know what hit them (literally).",
+        "icon": "images/BG3_Quest29.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest30",
+        "name": "Fists of Fury",
+        "description": "Kill a character with an Unarmed Strike.",
+        "icon": "images/BG3_Quest30.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest31",
+        "name": "Devil's in the Details ",
+        "description": "Defeat Commander Zhalk on the nautiloid.",
+        "icon": "images/BG3_Quest31.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest32",
+        "name": "Pest Control",
+        "description": "",
+        "icon": "images/BG3_Quest32.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest33",
+        "name": "A Grym Fate",
+        "description": "",
+        "icon": "images/BG3_Quest33.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest34",
+        "name": "Non-Invasive Procedure",
+        "description": "Kill the Surgeon before he performs surgery on you in combat.",
+        "icon": "images/BG3_Quest34.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest35",
+        "name": "Penny Pincher",
+        "description": "Defeat the Toll Collector without her using gold against you - excellent budgeting. ",
+        "icon": "images/BG3_Quest35.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest36",
+        "name": "No Free Lunches",
+        "description": "",
+        "icon": "images/BG3_Quest36.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest37",
+        "name": "Fancy Footwork",
+        "description": "Defeat Gortash in Wyrm's Rock without activating any traps.",
+        "icon": "images/BG3_Quest37.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest38",
+        "name": "First Blood",
+        "description": "",
+        "icon": "images/BG3_Quest38.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest39",
+        "name": "Interfectorem Draconis",
+        "description": "",
+        "icon": "images/BG3_Quest39.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest40",
+        "name": "Crash Landing",
+        "description": "In the Wyrmway, wait until the dragon is mid-flight, then knock it out of the sky - KAPOW.",
+        "icon": "images/BG3_Quest40.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest41",
+        "name": "Bottoms Up",
+        "description": "Long Rest using only alcohol - a time-honoured dwarven tradition. ",
+        "icon": "images/BG3_Quest41.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest42",
+        "name": "Shove Off",
+        "description": "Kill a creature with falling damage.",
+        "icon": "images/BG3_Quest42.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest43",
+        "name": "Bookworm",
+        "description": "Read 100 different books in a single playthrough. Adventuring isn't just daring quests, you know.",
+        "icon": "images/BG3_Quest43.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest44",
+        "name": "Punch Drunk",
+        "description": "Defeat twenty opponents while drunk - down them.",
+        "icon": "images/BG3_Quest44.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest45",
+        "name": "Fetch Quest",
+        "description": "Play fetch with Scratch - the best boy in the Realms.",
+        "icon": "images/BG3_Quest45.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest46",
+        "name": "Repairing the Weave",
+        "description": "",
+        "icon": "images/BG3_Quest46.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest47",
+        "name": "The Lich-Queen's Wrath",
+        "description": "",
+        "icon": "images/BG3_Quest47.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest48",
+        "name": "To Bloom in Darkest Night",
+        "description": "",
+        "icon": "images/BG3_Quest48.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest49",
+        "name": "Hot Date",
+        "description": "",
+        "icon": "images/BG3_Quest49.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest50",
+        "name": "Just a Nibble",
+        "description": "",
+        "icon": "images/BG3_Quest50.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest51",
+        "name": "Loophole",
+        "description": "",
+        "icon": "images/BG3_Quest51.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest52",
+        "name": "Embrace Your Urge",
+        "description": "",
+        "icon": "images/BG3_Quest52.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest53",
+        "name": "Critical Hit",
+        "description": "Complete the game in Tactician mode.",
+        "icon": "images/BG3_Quest53.jpg",
+        "tags": []
+    },
+    {
+        "id": "BG3_Quest54",
+        "name": "Foehammer",
+        "description": "Complete the game in Honour mode.",
+        "icon": "images/BG3_Quest54.jpg",
+        "tags": []
+    }
+];
