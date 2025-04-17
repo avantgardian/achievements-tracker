056c55a72de8bdeb9692bf48a1f0a26c9fc8cebb
Andrei Stratu <andrew.avantgardian@gmail.com>
2025-04-17 22:29:39 +0100

    Add Farming Simulator 25 achievements

---
+++ games/farming_simulator_25/js/achievements.js
@@ -0,0 +1,317 @@
+window.gameAchievements = [
+    {
+        "id": "CarDriving",
+        "name": "Road Trip",
+        "description": "That thing wouldn't have fit onto the street anyway.",
+        "icon": "images/CarDriving.jpg",
+        "tags": []
+    },
+    {
+        "id": "FertilizeFirst",
+        "name": "Plant get enough",
+        "description": "A little help can go a long way.",
+        "icon": "images/FertilizeFirst.jpg",
+        "tags": []
+    },
+    {
+        "id": "Fertilize",
+        "name": "The plot thickens",
+        "description": "You won't rest until you've reached maximum efficiency.",
+        "icon": "images/Fertilize.jpg",
+        "tags": []
+    },
+    {
+        "id": "HorseJumpsFirst",
+        "name": "Allez hopp",
+        "description": "Oh, they can do THAT as well?",
+        "icon": "images/HorseJumpsFirst.jpg",
+        "tags": []
+    },
+    {
+        "id": "HorseJumps",
+        "name": "You are not a kangaroo",
+        "description": "Seriously, think of the poor animal!",
+        "icon": "images/HorseJumps.jpg",
+        "tags": []
+    },
+    {
+        "id": "HorseRidingFirst",
+        "name": "Giddy-up!",
+        "description": "I could get used to getting around like this…",
+        "icon": "images/HorseRidingFirst.jpg",
+        "tags": []
+    },
+    {
+        "id": "HorseRiding",
+        "name": "Thoroughbred!",
+        "description": "I'm a real cowboy, mom!",
+        "icon": "images/HorseRiding.jpg",
+        "tags": []
+    },
+    {
+        "id": "MissionFirst",
+        "name": "Help me to help you",
+        "description": "Sure, I can take care of that, neighbor.",
+        "icon": "images/MissionFirst.jpg",
+        "tags": []
+    },
+    {
+        "id": "Mission",
+        "name": "Helper A does not stop … ever",
+        "description": "What do you mean \"take care of my own farm as well\"?",
+        "icon": "images/Mission.jpg",
+        "tags": []
+    },
+    {
+        "id": "PlayTime",
+        "name": "It's just the beginning",
+        "description": "I've never felt happier than here.",
+        "icon": "images/PlayTime.jpg",
+        "tags": []
+    },
+    {
+        "id": "SowFirst",
+        "name": "Just a sprinkle",
+        "description": "You have to think about the future.",
+        "icon": "images/SowFirst.jpg",
+        "tags": []
+    },
+    {
+        "id": "Sow",
+        "name": "It's sow easy",
+        "description": "Planting one bean will yield much green.",
+        "icon": "images/Sow.jpg",
+        "tags": []
+    },
+    {
+        "id": "HarvestedFirst",
+        "name": "Own use",
+        "description": "Reap what you sow.",
+        "icon": "images/HarvestedFirst.jpg",
+        "tags": []
+    },
+    {
+        "id": "Harvested",
+        "name": "Large-scale supplier",
+        "description": "These are the fruits of your labor.",
+        "icon": "images/Harvested.jpg",
+        "tags": []
+    },
+    {
+        "id": "TractorDriving",
+        "name": "Field Trip",
+        "description": "One vehicle for all of your needs.",
+        "icon": "images/TractorDriving.jpg",
+        "tags": []
+    },
+    {
+        "id": "TruckDriving",
+        "name": "Long haul",
+        "description": "In it for the long game, I see.",
+        "icon": "images/TruckDriving.jpg",
+        "tags": []
+    },
+    {
+        "id": "VehicleRepaint",
+        "name": "van Gogh",
+        "description": "Look at Mr. Fancypants over there.",
+        "icon": "images/VehicleRepaint.jpg",
+        "tags": []
+    },
+    {
+        "id": "VehicleRepairFirst",
+        "name": "It just fell off",
+        "description": "It was only a matter of time…",
+        "icon": "images/VehicleRepairFirst.jpg",
+        "tags": []
+    },
+    {
+        "id": "VehicleRepair",
+        "name": "Fix me up",
+        "description": "Seriously, you should consider a membership card.",
+        "icon": "images/VehicleRepair.jpg",
+        "tags": []
+    },
+    {
+        "id": "BreedSheep",
+        "name": "Fluffiness",
+        "description": "Building up my wool empire.",
+        "icon": "images/BreedSheep.jpg",
+        "tags": []
+    },
+    {
+        "id": "BreedPigs",
+        "name": "Three little piggies…",
+        "description": "You'll be amazed how much food they need.",
+        "icon": "images/BreedPigs.jpg",
+        "tags": []
+    },
+    {
+        "id": "BreedChicken",
+        "name": "Clucky Streak",
+        "description": "Now that's a lot of Easter eggs.",
+        "icon": "images/BreedChicken.jpg",
+        "tags": []
+    },
+    {
+        "id": "NumBeehives",
+        "name": "Bringing in the Honey",
+        "description": "Well you've been one busy bee, haven't you?",
+        "icon": "images/NumBeehives.jpg",
+        "tags": []
+    },
+    {
+        "id": "BreedCows",
+        "name": "Cowherd",
+        "description": "Bet you didn't think they'd be that much work, right?",
+        "icon": "images/BreedCows.jpg",
+        "tags": []
+    },
+    {
+        "id": "NumDrivables",
+        "name": "I like to switch it up",
+        "description": "We're slowly getting there.",
+        "icon": "images/NumDrivables.jpg",
+        "tags": []
+    },
+    {
+        "id": "NumVehiclesSmall",
+        "name": "This is just my weekend vehicle",
+        "description": "Yes, I need each and every one of these.",
+        "icon": "images/NumVehiclesSmall.jpg",
+        "tags": []
+    },
+    {
+        "id": "NumVehiclesLarge",
+        "name": "Vehicle fleet",
+        "description": "Ok, I might have a problem…",
+        "icon": "images/NumVehiclesLarge.jpg",
+        "tags": []
+    },
+    {
+        "id": "CultivateFirst",
+        "name": "I read Shakespeare and stuff",
+        "description": "The first step of any farmer.",
+        "icon": "images/CultivateFirst.jpg",
+        "tags": []
+    },
+    {
+        "id": "Cultivate",
+        "name": "Highly cultivated",
+        "description": "I'm a little proud of this one myself.",
+        "icon": "images/Cultivate.jpg",
+        "tags": []
+    },
+    {
+        "id": "PetDog",
+        "name": "Ultimutt Pawesomeness",
+        "description": "Who's a good boy?",
+        "icon": "images/PetDog.jpg",
+        "tags": []
+    },
+    {
+        "id": "WrappedBales",
+        "name": "That's a wrap",
+        "description": "What's in the bale?",
+        "icon": "images/WrappedBales.jpg",
+        "tags": []
+    },
+    {
+        "id": "CottonBales",
+        "name": "Gone but not for cotton",
+        "description": "Where would you even store them?!",
+        "icon": "images/CottonBales.jpg",
+        "tags": []
+    },
+    {
+        "id": "DeliveryGrapes",
+        "name": "Raisin the stakes",
+        "description": "It's Californian gold!",
+        "icon": "images/DeliveryGrapes.jpg",
+        "tags": []
+    },
+    {
+        "id": "DeliveryOlives",
+        "name": "Olea europaea",
+        "description": "It's a fruit, not a vegetable!",
+        "icon": "images/DeliveryOlives.jpg",
+        "tags": []
+    },
+    {
+        "id": "DeliverySorghum",
+        "name": "Original grain",
+        "description": "My horses are crazy for it.",
+        "icon": "images/DeliverySorghum.jpg",
+        "tags": []
+    },
+    {
+        "id": "DeliveryStones",
+        "name": "Rock on",
+        "description": "The path to success is paved with rocks.",
+        "icon": "images/DeliveryStones.jpg",
+        "tags": []
+    },
+    {
+        "id": "CutTreeFirst",
+        "name": "I'm stumped",
+        "description": "If a tree falls in a forest and no one is around to hear it, does one hand clap?",
+        "icon": "images/CutTreeFirst.jpg",
+        "tags": []
+    },
+    {
+        "id": "CutTree",
+        "name": "You wood not believe it",
+        "description": "Taking a leaf of absence.",
+        "icon": "images/CutTree.jpg",
+        "tags": []
+    },
+    {
+        "id": "Money",
+        "name": "Hard work pays off",
+        "description": "You know you can also spend it, right?",
+        "icon": "images/Money.jpg",
+        "tags": []
+    },
+    {
+        "id": "NumPlaceables",
+        "name": "All out of Land",
+        "description": "Everything the light touches...is mine.",
+        "icon": "images/NumPlaceables.jpg",
+        "tags": []
+    },
+    {
+        "id": "NumProductionPoints",
+        "name": "Well-Oiled Machine",
+        "description": "Hmm, we could franchise this…",
+        "icon": "images/NumProductionPoints.jpg",
+        "tags": []
+    },
+    {
+        "id": "CollectiblesUS",
+        "name": "Rusty",
+        "description": "The Museum needs help.",
+        "icon": "images/CollectiblesUS.jpg",
+        "tags": []
+    },
+    {
+        "id": "CollectiblesAS",
+        "name": "Over 9000!",
+        "description": "With the power of farm balls for even more yield.",
+        "icon": "images/CollectiblesAS.jpg",
+        "tags": []
+    },
+    {
+        "id": "CollectiblesEU",
+        "name": "Tutti-Frutti",
+        "description": "The legend of the golden apple.",
+        "icon": "images/CollectiblesEU.jpg",
+        "tags": []
+    },
+    {
+        "id": "LoadedOldSavegame",
+        "name": "It's never too late to farm",
+        "description": "It feels good to be home.",
+        "icon": "images/LoadedOldSavegame.jpg",
+        "tags": []
+    }
+];
